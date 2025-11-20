f67da79ef9701426c6560f81742e707e11bccf8b
Yaacoub Tantaoui <tonemail@example.com>
2025-11-20 21:12:11 +0100

    blur

--- PythonCode.py
+++ PythonCode.py
@@ -11,6 +11,7 @@ def factoriel(nombre :int )->int :
  
 def get_age(name :str)->int :
    print("ok")
+   print("hello")
    dataset= pd.read_csv("C:/Users/PC/Documents/DataFactory/Code/name_age.csv")
    return dataset
 
